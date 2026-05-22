e664b9cb58877920b9ac3403210c5b659674c7fe
TiavinaH <tharivonjy@gmail.com>
2026-05-22 16:40:26 +0300

    maj

--- src/js/app.js
+++ src/js/app.js
@@ -398,7 +398,7 @@ function load() {
 
   setTimeout(() => {
     sectionMenu.style.opacity = "1";
-    sectionMenu.style.transform = "translateY(0)";
+    sectionMenu.style.transform = "translateY(10%)";
   }, 2550);
 }
 
